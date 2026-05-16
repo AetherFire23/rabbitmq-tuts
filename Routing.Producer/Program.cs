df0ab9dce325c4561837cb218625d3f955c4ece2
f3d <108708015+AetherFire23@users.noreply.github.com>
2026-05-16 13:51:57 -0400

    ADd topics

---
+++ Routing.Producer/Program.cs
@@ -0,0 +1,24 @@
+﻿using System.Text;
+using RabbitMQ.Client;
+
+/*
+ * Can launch this program with arugments to only send to a specific routingKey
+ * ex: dotnet run info
+ */
+ConnectionFactory factory = new ConnectionFactory { HostName = "localhost" };
+using IConnection connection = await factory.CreateConnectionAsync();
+using IChannel channel = await connection.CreateChannelAsync();
+
+/*
+ * The big difference here is the Direct route option.
+ * I can choose a routing key (important). Then you can submit message only to consumers that have a certain routing key. 
+ */
+await channel.ExchangeDeclareAsync(exchange: "direct_logs", type: ExchangeType.Direct);
+
+string severity = (args.Length > 0) ? args[0] : "info";
+string message = (args.Length > 1) ? string.Join(" ", args.Skip(1).ToArray()) : "Hello World!";
+await channel.BasicPublishAsync(exchange: "direct_logs", routingKey: severity, body: Encoding.UTF8.GetBytes(message));
+Console.WriteLine($" [x] Sent '{severity}':'{message}'");
+
+Console.WriteLine(" Press [enter] to exit.");
+Console.ReadLine();
---
+++ Topics.Producer/Program.cs
@@ -0,0 +1,29 @@
+﻿using RabbitMQ.Client;
+using System.Text;
+
+/*
+ * Sets of working program launch settings:
+ *
+ * Consumer : dotnet run anonymous.info
+ * Producer : dotnet run
+ *
+ * Consumer : dotnet run anonymous.*
+ * Producer : dotnet run
+ *
+ * Consumer : dotnet run *.info
+ * Producer : dotnet run
+ *
+ */
+
+
+var factory = new ConnectionFactory { HostName = "localhost" };
+using var connection = await factory.CreateConnectionAsync();
+using var channel = await connection.CreateChannelAsync();
+
+await channel.ExchangeDeclareAsync(exchange: "topic_logs", type: ExchangeType.Topic);
+
+var routingKey = (args.Length > 0) ? args[0] : "anonymous.info";
+var message = (args.Length > 1) ? string.Join(" ", args.Skip(1).ToArray()) : "Hello World!";
+var body = Encoding.UTF8.GetBytes(message);
+await channel.BasicPublishAsync(exchange: "topic_logs", routingKey: routingKey, body: body);
+Console.WriteLine($" [x] Sent '{routingKey}':'{message}'");
